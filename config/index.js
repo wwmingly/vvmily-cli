e1625eead2ee2598b22b4a54b7f4db484240f526
wwmingly <2991374415@qq.com>
2021-09-01 08:48:08 +0800

    feat: add template

--- config/index.js
+++ config/index.js
@@ -9,16 +9,21 @@ const defaultUrlList = [
     },
     {
         key: "b",
+        name: "vue-admin-simple-template后台简易模板",
+        value: "direct:https://github.com/wwmingly/vue-admin-simple-template",
+    },
+    {
+        key: "c",
         name: "antd-admin后台管理框架",
         value: "direct:https://github.com/zuiidea/antd-admin",
     },
     {
-        key: "c",
+        key: "d",
         name: "vue-admin-template之基础模板",
         value: "direct:https://github.com/PanJiaChen/vue-admin-template",
     },
     {
-        key: "d",
+        key: "e",
         name: "vue-admin-template之集成方案",
         value: "direct:https://github.com/PanJiaChen/vue-element-admin",
     }
